82758538907cda6b0ae2b6ca627ca207bd01c236
karokuusikallio <karokuusikallio@gmail.com>
2023-01-25 14:35:19 +0200

    added station endpoint for api

---
+++ src/pages/api/station.ts
@@ -0,0 +1,132 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import prismaClient from "../../client";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { stationId, searchWord, skip, take, orderColumn, order } = req.query;
+
+  let ordering = null;
+  if (orderColumn && order) {
+    ordering = {
+      [orderColumn as string]: order as string,
+    };
+    console.log(ordering);
+  }
+
+  if (req.method === "GET" && stationId) {
+    try {
+      const station = await prismaClient.station.findFirst({
+        where: {
+          id: Number(stationId),
+        },
+      });
+
+      const departures = await prismaClient.journey.findMany({
+        where: {
+          departureStationId: Number(stationId),
+        },
+      });
+
+      const returns = await prismaClient.journey.findMany({
+        where: {
+          returnStationId: Number(stationId),
+        },
+      });
+
+      return res.status(200).json({
+        departures: departures.length,
+        returns: returns.length,
+        ...station,
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(400).json({ error });
+    }
+  }
+
+  if (req.method === "GET" && searchWord) {
+    try {
+      const result = await prismaClient.station.findMany({
+        where: {
+          OR: [
+            {
+              nameFi: {
+                contains: searchWord as string,
+                mode: "insensitive",
+              },
+            },
+            {
+              nameSwe: {
+                contains: searchWord as string,
+                mode: "insensitive",
+              },
+            },
+            {
+              nameEng: {
+                contains: searchWord as string,
+                mode: "insensitive",
+              },
+            },
+            {
+              addressFi: {
+                contains: searchWord as string,
+                mode: "insensitive",
+              },
+            },
+            {
+              addressSwe: {
+                contains: searchWord as string,
+                mode: "insensitive",
+              },
+            },
+            {
+              cityFi: {
+                contains: searchWord as string,
+                mode: "insensitive",
+              },
+            },
+            {
+              citySwe: {
+                contains: searchWord as string,
+                mode: "insensitive",
+              },
+            },
+            {
+              operator: {
+                contains: searchWord as string,
+                mode: "insensitive",
+              },
+            },
+          ],
+        },
+        orderBy: ordering || {
+          id: "asc",
+        },
+      });
+
+      return res.status(200).json({
+        result,
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(400).json({ error });
+    }
+  }
+
+  if (req.method === "GET") {
+    try {
+      const stations = await prismaClient.station.findMany({
+        skip: Number(skip) || 0,
+        take: Number(take) || 10,
+      });
+
+      return res.status(200).json(stations);
+    } catch (error) {
+      console.log(error);
+      return res.status(400).json({ error });
+    }
+  }
+
+  return res.status(400).json({ error: "Request method not supported." });
+};
+
+export default handler;
